0e2fc989619f939665eea0be58da3a52a25479b7
Eugene <aethelz@users.noreply.github.com>
2021-06-06 16:37:08 +0300

    Setup deployment on GitHub Pages, switch to HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 import './index.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ReactQueryDevtools } from 'react-query/devtools';
